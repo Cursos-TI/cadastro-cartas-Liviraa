0a0848615189484e44a39cd17d069ed62a13dadf
Liviraa <liviramski17@gmail.com>
2025-10-28 22:32:06 +0000

    Inclusão de  carta 1 e carta 2

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -5,8 +5,8 @@
 // Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
 
 int main() {
-  // Área para definição das variáveis para armazenar as propriedades das cidades
- 
+ // Carta 1
+
   int carta = 1;
   char codigo[10] = "A01";  
   char estado[50] = "São Paulo";
@@ -26,5 +26,30 @@ int main() {
   printf("PIB: %.2lf trilhões de reais\n", pib);
   printf("Pontos Turísticos: %d\n", pontosTuristicos);
 
+
+// Carta 2
+
+  int carta = 2;
+  char codigo[10] = "A02";  
+  char estado[50] = "Rio de Janeiro";
+  char cidade [50] = "Rio de Janeiro";
+  float populacao = 16.055;     // em milhões
+  float area = 44000.0;    // em km²
+  double pib = 1.3;        // em trilhões de reais
+  int pontosTuristicos = 50;
+
+  // Área para entrada de dados
+
+  printf("Código: %s\n", codigo);
+  printf("Carta: %d\n", carta);
+  printf("Estado: %s\n", estado);
+  printf("Cidade: %s\n", cidade);
+  printf("População: %.1f milhões\n", populacao);
+  printf("Área: %.0f km²\n", area);
+  printf("PIB: %.2lf trilhões de reais\n", pib);
+  printf("Pontos Turísticos: %d\n", pontosTuristicos);
+
 return 0;
-} 
+
+}
+
