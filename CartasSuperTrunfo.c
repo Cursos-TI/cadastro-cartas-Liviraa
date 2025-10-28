becd4be909440424f3b5f91a179491fc7de2a9bb
Liviraa <liviramski17@gmail.com>
2025-10-28 22:04:42 +0000

    Inclusão de carta 01 Super Trunfo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,8 +6,26 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
+ 
+  int carta = 1;
+  char codigo[10] = "A01";  
+  char estado[50] = "São Paulo";
+  char cidade [50] = "São Paulo";
+  float populacao = 44.4;     // em milhões
+  float area = 240000.0;    // em km²
+  double pib = 3.13;        // em trilhões de reais
+  int pontosTuristicos = 47;
+ 
+    // Área para entrada de dados
+  printf("Código: %s\n", codigo);
+  printf("Carta: %d\n", carta);
+  printf("Estado: %s\n", estado);
+  printf("Cidade: %s\n", cidade);
+  printf("População: %.1f milhões\n", populacao);
+  printf("Área: %.0f km²\n", area);
+  printf("PIB: %.2lf trilhões de reais\n", pib);
+  printf("Pontos Turísticos: %d\n", pontosTuristicos);
 
-  // Área para entrada de dados
 
   // Área para exibição dos dados da cidade
 
